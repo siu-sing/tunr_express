f7fed8cf6618635f7cc7dbcc69c5cfcc183efe40
siu-sing <lai.siusing@gmail.com>
2020-07-13 23:41:10 +0800

    first push

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'siusing',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    port: 5432,
 };
 
 const pool = new pg.Pool(configs);
 
 pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+    console.log('idle client error', err.message, err.stack);
 });
 
 /**
@@ -30,7 +30,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(methodOverride('_method'));
@@ -48,16 +48,32 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (request, response) => {
-  // query database for all pokemon
+const makeQuery = async function (query, values) {
+    try {
+        console.log("entered make query");
+        let results = await pool.query(query, values);
+        return results.rows;
+    } catch (err) {
+        return err;
+    }
+};
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+app.get('/', (request, response) => {
+    // query database for all pokemon
+    // let res = await makeQuery("select * from users;")
+    // console.log(res);
+    // respond with HTML page displaying all pokemon
+    response.render('home');
 });
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
+app.get('/new', async (request, response) => {
+    // respond with HTML page with form to create new pokemon
+    
+    console.log("query starting");
+    let res = await makeQuery("select * from users");
+    console.log(res);
+    console.log("query ended");
+    response.render('new');
 });
 
 
@@ -68,16 +84,16 @@ app.get('/new', (request, response) => {
  */
 const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
+let onClose = function () {
+
+    console.log("closing");
+
+    server.close(() => {
+
+        console.log('Process terminated');
+
+        pool.end(() => console.log('Shut down db connection pool'));
+    })
 };
 
 process.on('SIGTERM', onClose);
